4f6876497e779f05ec9983597b6a750a5afd7ba8
Jose Aparcana <josered30@hotmail.com>
2021-08-21 05:29:41 -0500

    .

---
+++ build/CMakeFiles/manager.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/joser/Desktop/password-manager/src/encryption.cpp" "CMakeFiles/manager.dir/src/encryption.cpp.obj" "gcc" "CMakeFiles/manager.dir/src/encryption.cpp.obj.d"
+  "C:/Users/joser/Desktop/password-manager/src/generator.cpp" "CMakeFiles/manager.dir/src/generator.cpp.obj" "gcc" "CMakeFiles/manager.dir/src/generator.cpp.obj.d"
+  "C:/Users/joser/Desktop/password-manager/src/main.cpp" "CMakeFiles/manager.dir/src/main.cpp.obj" "gcc" "CMakeFiles/manager.dir/src/main.cpp.obj.d"
+  "C:/Users/joser/Desktop/password-manager/src/manager.cpp" "CMakeFiles/manager.dir/src/manager.cpp.obj" "gcc" "CMakeFiles/manager.dir/src/manager.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/manager.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/manager.dir/src/encryption.cpp.obj"
+  "CMakeFiles/manager.dir/src/encryption.cpp.obj.d"
+  "CMakeFiles/manager.dir/src/generator.cpp.obj"
+  "CMakeFiles/manager.dir/src/generator.cpp.obj.d"
+  "CMakeFiles/manager.dir/src/main.cpp.obj"
+  "CMakeFiles/manager.dir/src/main.cpp.obj.d"
+  "CMakeFiles/manager.dir/src/manager.cpp.obj"
+  "CMakeFiles/manager.dir/src/manager.cpp.obj.d"
+  "libmanager.dll.a"
+  "manager.exe"
+  "manager.exe.manifest"
+  "manager.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
